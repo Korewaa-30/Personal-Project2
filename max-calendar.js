6ed5fdab73755b3cec15d11d6a3a9d30fa308981
Korewaa-30 <dukersarah30@gmail.com>
2023-11-09 07:46:56 +0000

    update

--- max-calendar.js
+++ max-calendar.js
@@ -1,7 +1,24 @@
 let timeElement = document.querySelector("#time");
 let dateElement = document.querySelector("#date");
+let dayElement = document.querySelector("#day");
+let monthElement = document.querySelector("#month-picker");
+let calendarElement = document.querySelector("#calendar");
+let month_list_list = document.querySelector(".month-list");
 
 let today = new Date();
+let desiredMonth = today.getMonth();
+let desiredYear = today.getFullYear();
+
+let day = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+];
+dayElement.innerHTML = day[today.getDay()];
 
 let hours = today.getHours();
 let amOrPm = hours >= 12 ? "PM" : "AM";
@@ -51,3 +68,70 @@ let month = [
 dateElement.innerHTML = `${ordinalDay} ${
 	month[today.getMonth()]
 }, ${today.getFullYear()}`;
+
+function isLeapYear(year) {
+	return (
+		(year % 4 === 0 && year % 100 !== 0) ||
+		(year % 100 === 0 && year % 400 === 0)
+	);
+}
+
+function getFebDays(year) {
+	return isLeapYear(year) ? 29 : 28;
+}
+
+function selectMonth() {
+	month_list_list.classList.remove("hideonce");
+	month_list_list.classList.remove("hide");
+	month_list_list.classList.add("show");
+}
+
+monthElement.addEventListener("click", selectMonth);
+
+function generateCalendar(selectedMonth, year) {
+	let calendarDays = document.querySelector("#days");
+	calendarDays.innerHTML = "";
+
+	let calendarYear = document.querySelector("#year");
+	let monthDays = [
+		31,
+		getFebDays(year),
+		31,
+		30,
+		31,
+		30,
+		31,
+		31,
+		30,
+		31,
+		30,
+		31,
+	];
+
+	monthElement.innerHTML = selectedMonth[month];
+	calendarYear.innerHTML = year;
+	let firstDay = new Date(year, month, 1);
+
+	for (let i = 0; i < firstDay.getDay(); i++) {
+		let emptyDay = document.createElement("div");
+		emptyDay.classList.add("empty-day");
+		calendarDays.appendChild(emptyDay);
+	}
+
+	for (let i = 0; i <= monthDays[month] + firstDay.getDay() - 1; i++) {
+		let day = document.createElement("div");
+		if (i >= firstDay.getDay()) {
+			day.innerHTML = i - firstDay.getDay() + 1;
+			if (
+				i - firstDay.getDay() + 1 === today.getDate() &&
+				year === today.getFullYear() &&
+				month === today.getMonth()
+			) {
+				day.classList.add("current-date");
+			}
+		}
+		calendarDays.appendChild(day);
+	}
+}
+
+generateCalendar(desiredMonth, desiredYear);
